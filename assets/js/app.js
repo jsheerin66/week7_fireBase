e25efad57ff3db353e13984130d969e9d94d237e
jsheerin66 <justinsheerin@gmail.com>
2017-05-07 22:18:13 -0400

    commit before debug
    commit before debugging

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,14 +19,55 @@ $(document).on("ready", function() {
         var startTime = $("#startTime").val().trim();
         var frequency = $("#frequency").val().trim();
 
-        var newtrain = {
+        var newTrain = {
             name: trainName,
             destination: destination,
+            //will firstTrain throw it off?
             firstTrain: startTime,
             frequency: frequency,
         }
 
         trainData.ref().push(newTrain);
+
+        console.log(newTrain.name)
+        console.log(newTrain.destination)
+        console.log(newTrain.firstTrain)
+        console.log(startTime)
+        console.log(newTrain.frequency)
+
+        $("#trainName").empty();
+        $("#destination").empty();
+        $("#startTime").empty();
+        $("frequency").empty();
+
+        return false;
+
+
+    });
+
+    trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+        console.log(childSnapshot.val());
+
+        var trainNameAppend = childSnapshot.val().name
+        var destinationAppend = childSnapshot.val().destination
+        var firstTrainAppend = childSnapshot.val().firstTrain
+        var frequencyAppend = childSnapshot.val().frequency
+
+        var timeDifference = moment().diff(moment.unix(timeFirstTrain), "minutes");
+
+        var remainingTime = moment().diff(moment.unix(timeFirstTrain), "minutes") % frequencyAppend;
+        var timeMinutes = frequencyAppend - rainingTime;
+
+
+        var arrivalTime = moment().add(timeMinutes, "m").format("hh:mm A");
+        console.log(arrivalTime)
+        console.log(timeMinutes)
+
+        console.log(moment().format("hh:mm A"))
+        console.log(momonet().format("X"))
+
+        $("#tableBody").append("<tr><td>" + trainNameAppend + "</td><td>" + destinationAppend + "</td><td>" + firstTrainAppend +  "</td><td>" + arrivalTime + "</td><td>" + timeMinutes + "</td></tr>")
 
     });
 
